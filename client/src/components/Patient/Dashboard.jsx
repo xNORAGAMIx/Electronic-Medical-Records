ecb5b5605928e7b24c302cd93a8c4618b8a58d71
Manas Das <noragami0021@gmail.com>
2025-04-29 14:35:22 +0530

    Booking and sharing access

--- client/src/components/Patient/Dashboard.jsx
+++ client/src/components/Patient/Dashboard.jsx
@@ -19,13 +19,11 @@ import {
 //image
 import patientLogo from "../../../public/DJV MAR 1012-04.jpg";
 
-// set up initials
+// set up constants
 const contractABI = PatientRegistration.abi;
 const contractAddress = PATIENT_CONTRACT_ADDRESS;
 const privateKey = PRIVATE_KEY;
 
-//0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-//0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d
 const uploadABI = Upload.abi;
 const uploadAddress = UPLOAD_CONTRACT_ADDRESS;
 
